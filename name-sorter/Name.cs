71c9c60c888468fc9574c4ec4b24013908f4cc58
bdsipon <sipon.here@gmail.com>
2017-11-11 03:05:45 +1000

    Updated

--- name-sorter/Name.cs
+++ name-sorter/Name.cs
@@ -9,13 +9,15 @@ namespace name_sorter
    public class Name
     {
         public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string SecondName { get; set; }
         public string OtherName { get; set; }
-        public string FourthName { get; set; }
+        public string LastName { get; set; }
+      
+      
 
         public override string ToString()
         {
-            return $"{FirstName}{LastName}{OtherName}{FourthName}".Trim();
+            return $"{FirstName}{SecondName}{OtherName}{LastName}".Trim();
         }
     }
 }
--- name-sorter/NameSorter.cs
+++ name-sorter/NameSorter.cs
@@ -8,5 +8,14 @@ namespace name_sorter
 {
     class NameSorter
     {
+        public IEnumerable<Name> SortNamesbyLast(IEnumerable<Name> name)
+        {
+            IEnumerable<Name> names = name.OrderBy(n => n.LastName)
+                .ThenBy(n => n.OtherName)
+                 .ThenBy(n => n.SecondName)
+                  .ThenBy(n => n.FirstName);
+            return names;
+
+        }
     }
 }
