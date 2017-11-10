d3b040b62a88c71a026faead7af33f43c56ac597
bdsipon <sipon.here@gmail.com>
2017-11-10 19:37:36 +1000

    updated the NameWriter

--- name-sorter/NameWritterToFile.cs
+++ name-sorter/NameWritterToFile.cs
@@ -8,5 +8,19 @@ namespace name_sorter
 {
     class NameWritterToFile
     {
+        private string outputFileName = "";
+        public NameWritterToFile(string outputFile)
+        {
+            this.outputFileName = outputFile;
+        }
+        public string OutputFileName
+        {
+
+            get { return outputFileName; }
+            set
+            {
+                outputFileName = value;
+            }
+        }
     }
 }
