9eb6192195a387e1c8d93b426f31f210c4fece8b
bdsipon <sipon.here@gmail.com>
2017-11-11 12:14:10 +1000

    ListProcessor Updated

--- name-sorter/ListProcessor.cs
+++ name-sorter/ListProcessor.cs
@@ -6,7 +6,75 @@ using System.Threading.Tasks;
 
 namespace name_sorter
 {
-    class ListProcessor
+    public class ListProcessor
     {
+        // the method remove empty strings from list
+        public List<string> purgeList(List<string>list)
+        {
+            list = (from l in list where !string.IsNullOrEmpty(l) select l).ToList();
+            return list;
+        }
+        //the method rearranges the a specific name entity to a sortable entity
+        public Name listToSortableName(List<string> list)
+        {
+            var name = new Name();
+
+            try
+            {
+                name.LastName = list[list.Count - 1];
+                name.OtherName = list[list.Count - 2];
+                name.SecondName = list[list.Count - 3];
+                name.FirstName = list[list.Count - 4];
+
+            }
+            catch
+            {
+
+            }
+            return name;
+        }
+
+        // reverse the sorted names to Normal name
+        public IEnumerable<Name> reverseNamestoNormal(IEnumerable<Name> Names)
+        {
+            return Names.Select(reverseName);
+        }
+
+        private Name reverseName(Name name)
+        {
+            var nList = nameToList(name);
+            return listToName(nList);
+        }
+        private Name listToName(List<string> list)
+        {
+            Name name = new Name();
+            try
+            {
+                name.FirstName = list[list.Count - 1];
+                name.SecondName = list[list.Count - 2];
+                name.OtherName = list[list.Count - 3];
+                name.LastName = list[list.Count - 4];
+            }
+            catch
+            {
+
+            }
+            return name;
+        }
+        //convert name to list 
+        private List<string> nameToList(Name name)
+        {
+            var result = new List<string>();
+
+            result.Add(name.LastName.Trim());
+            if (!string.IsNullOrEmpty(name.OtherName))
+                result.Add(name.OtherName.Trim());
+            if (!string.IsNullOrEmpty(name.SecondName))
+                result.Add(name.SecondName.Trim());
+            if (!string.IsNullOrEmpty(name.FirstName))
+                result.Add(name.FirstName.Trim());
+
+            return result;
+        }
     }
 }
